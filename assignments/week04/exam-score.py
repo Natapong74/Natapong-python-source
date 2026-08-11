54fd34e4cb70ed7fa23076af035f3574f6b46707
Natapong khetsakul <nataopong.k@ku.th>
2026-08-11 14:12:54 +0700

    fix exam-score.py

--- assignments/week04/exam-score.py
+++ assignments/week04/exam-score.py
@@ -4,7 +4,7 @@ for i in range(5):
     scores.append(score)
 for i in range(len(scores)):
     score = scores[i]
-    if score <= 50:
-        print(f"student {i+1} : {scores} Pass")
+    if score >= 50:
+        print(f"student {i+1} : {score} Pass")
     else:
-        print(f"student {i+1} : {scores} Not")
+        print(f"student {i+1} : {score} Not")
